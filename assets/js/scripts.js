f707dd7e4a20e7f7dd080ddec48b113b0f720eea
adeniji-adebanjo <aeben.adebanjo@gmail.com>
2024-07-15 10:33:23 +0100

    first commit for class and changes

---
+++ assets/js/scripts.js
@@ -0,0 +1,4 @@
+// This script can be used to add dynamic functionality if needed in the future.
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Page loaded successfully");
+});
